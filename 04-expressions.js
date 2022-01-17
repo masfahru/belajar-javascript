4b393a71551f5f0635a59647df98dffab04fd136
Mas Fahru <imam@fahru.my.id>
2022-01-17 08:30:34 +0700

    Expressions

---
+++ 04-expressions.js
@@ -0,0 +1,114 @@
+// Disadur dari
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators
+
+/**
+ * Expressions adalah kode yang menghasilkan dan atau memodifikasi suatu nilai
+ * 
+ * Kategorinya terbagi menjadi
+ * 
+ *  Primary Expression
+ *  Operator aritmatika
+ *  Operator Assignments
+ *  Increment & Decrement
+ *  Unary Operator
+ *  Operator relasi
+ *  Operator Persamaan
+ *  Operator Logika
+ *  Operator Ternary
+ * 
+ */
+
+/**
+ *  Primary Expression adalah expression dasar yang ada di Javascript
+ *  
+ *  Beberapa mungkin sama dengan statements, namun kita akan bahas perbedaannya
+ *  di masing-masing subbab nantinya
+ * 
+ * 
+ *      function
+ *      class
+ *      async function
+ *      await
+ *      this
+ *      [] ini adalah array literal
+ *      {} ini adalah object literal
+ *      () ini adalah grouping operator
+ *      property accessor
+ *      ... (spread operator)
+ * 
+ */
+
+/**
+ * Operator Aritmatika
+ *      +
+ *      -
+ *      /
+ *      *
+ *      %
+ *      **
+ */
+
+/**
+ * Operator Assignments:
+ *      =
+ *      *=
+ *      **=
+ *      /=
+ *      %=
+ *      +=
+ *      -=
+ *
+ *      Destructuring assignment
+ *      Comma operator
+ * */
+
+/**
+ * Increment & Decrement: penambahan atau pengurangan 1 dari nilai sebelumnya
+ *      A++
+ *      A--
+ *      ++A
+ *      --A
+ */
+
+/**
+ * Unary Operator adalah operator dengan sebuah operand saja
+ *      delete
+ *      typeof
+ *      +       (mengubah tipe data lain menjadi number)
+ *      -       (mengubah tipe data menjadi number negasinya)
+ *      !       (not)
+ */
+
+/**
+ * Operator Relasi
+ *  
+ *       in
+ *       <
+ *       >
+ *       <=
+ *       >=
+ */
+
+/**
+ * Operator Persamaan
+ *  
+ *       ==
+ *       !=
+ *       ===
+ *       !==
+ */
+
+
+/**
+ * Operator Logika
+ *  
+ *       &&
+ *       ||
+ *       ?? (null and undefined operator)
+ * */
+
+/**
+ * Operator Conditional Ternary (if else sebaris)
+ * 
+ *      (condition ? ifTrue : ifFalse)
+ */

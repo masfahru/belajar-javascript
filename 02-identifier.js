c0209278bf570d9309095ea944412a210df01c39
Mas Fahru <imam@fahru.my.id>
2022-01-17 08:30:10 +0700

    Materi Identifier

---
+++ 02-identifier.js
@@ -0,0 +1,42 @@
+// Case-Sensitive
+var myVariable = 1;
+var myvariable = 2;
+
+// Format variable name -> camelCase
+// GOOD
+var personName = "Muhammad Abdurrahman";
+var personAge = 16;
+
+// BAD
+var personname = "Muhammad Abdurrahman";
+var personage = 16;
+
+// remember butterfly is not the same as butterFly
+
+// Format constant name -> SCREAMING_SNAKE_CASE
+const NUMBER_OF_CORES = 8;
+
+// Format class name -> PascalCase
+class ExampleClass {
+    constructor() { }
+}
+
+// Reserved Keywords (Forbidden Keyword for naming object)
+/**
+ * abstract	arguments	await	boolean
+ * break	byte	case	catch
+ * char	class	const	continue
+ * double	else	enum	eval
+ * export	extends	false	final
+ * finally	float	for	function
+ * goto	if	implements	import
+ * in	instanceof	int	interface
+ * let	long	native	new
+ * null	package	private	protected
+ * public	return	short	static
+ * super	switch	synchronized	this
+ * throw	throws	transient	true
+ * try	typeof	var	void
+ * volatile	while	with	yield
+ * */ 
+

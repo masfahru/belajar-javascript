b4317b792b8ab3b83d7e444f9773773a90846d41
Mas Fahru <imam@fahru.my.id>
2022-01-17 08:30:25 +0700

    Statements

---
+++ 03-statements.js
@@ -0,0 +1,65 @@
+// Disadur dari
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements
+
+/**
+ * Statements adalah kode yang berisi tugas (task) untuk dijalankan oleh Mesin
+ * Perbedaan dengan Expression yakni statements tidak menghasilkan suatu nilai
+ * 
+ * Kategori Statements:
+ * Declaration
+ * Control Flow
+ * Function and Class
+ * Iteration
+ * Export Import
+ * 
+ * Materi di bawah akan dibahas pada masing-masing subbab
+ * 
+ * */
+
+/**
+ * Declarations
+ * 
+ *  var
+ *  let
+ *  const
+ */
+
+/**
+ * Control Flow
+ * 
+ *  block
+ *  break
+ *  continue
+ *  if..else
+ *  switch
+ *  try...catch
+ *  throw
+ */
+
+/**
+ * Function & class
+ * 
+ *  function 
+ *  async function
+ *  return
+ *  class
+ */
+
+/**
+ * Iterations
+ * 
+ *  do..while
+ *  for
+ *  for...in
+ *  for...of
+ *  for await...of
+ *  while
+ */
+
+/** 
+ * Export & Import
+ * 
+ *  import
+ *  export
+ */
+
